6fdf99c81849447f2442ce2c1062d1bdf01539c9
alysson-zanette <alysson-zanette@procergs.rs.gov.br>
2019-02-12 11:22:38 -0200

    Adicionado construção de base de dados em um único arquivo

--- src/main/resources/db/migration/V1.0__Create_Especie_Panta.sql
+++ src/main/resources/db/migration/V1.0__Create_Especie_Panta.sql
@@ -1,4 +1,4 @@
-CREATE SEQUENCE especie_planta_id_seq
+CREATE SEQUENCE IF NOT EXISTS especie_planta_id_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
@@ -6,7 +6,7 @@ CREATE SEQUENCE especie_planta_id_seq
   CACHE 1;
 
 CREATE TABLE IF NOT EXISTS especie_planta (
-    id INTEGER DEFAULT nextval('especie_planta_id_seq'),
+    id INT8 DEFAULT nextval('especie_planta_id_seq'),
     nome VARCHAR(100) NOT NULL ,
     umidade_minima FLOAT NOT NULL ,
     umidade_maxima FLOAT NOT NULL ,
@@ -21,3 +21,36 @@ VALUES (nextval('especie_planta_id_seq'), 'Morango',  60.00, 80.00, 'https://www
        (nextval('especie_planta_id_seq'), 'Cogumelo', 80.00, 90.00, 'https://www.zigg.com.br/static/img/no-imagem.png'),
        (nextval('especie_planta_id_seq'), 'Violeta',  40.00, 80.00, 'https://www.zigg.com.br/static/img/no-imagem.png'),
        (nextval('especie_planta_id_seq'), 'Lavanda',  20.00, 30.00, 'https://www.zigg.com.br/static/img/no-imagem.png');
+
+
+CREATE SEQUENCE IF NOT EXISTS planta_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+CREATE TABLE IF NOT EXISTS planta (
+                                    id INT8 DEFAULT nextval('planta_id_seq'),
+                                    codigo_sensor VARCHAR(100) NOT NULL ,
+                                    especie_planta_id INT8 NOT NULL,
+
+                                    CONSTRAINT planta_id_pk PRIMARY KEY (id),
+                                    CONSTRAINT especie_planta_id_fk FOREIGN KEY (especie_planta_id) REFERENCES especie_planta(id)
+);
+
+CREATE SEQUENCE IF NOT EXISTS historico_umidade_planta_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+CREATE TABLE IF NOT EXISTS historico_umidade_planta (
+                                                      id INT8 DEFAULT nextval('historico_umidade_planta_id_seq'),
+                                                      planta_id INT8 NOT NULL,
+                                                      data DATE,
+
+                                                      CONSTRAINT historico_umidade_planta_id_pk PRIMARY KEY (id),
+                                                      CONSTRAINT planta_id_fk FOREIGN KEY (planta_id) REFERENCES planta(id)
+);
